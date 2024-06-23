0e889845dcdb552d84eac4f4650d7a3dd5669da0
dianyo <b03902059@ntu.edu.tw>
2024-06-23 06:43:09 +0000

    add per channel 3d plot

--- classification/plot_utils.py
+++ classification/plot_utils.py
@@ -1,5 +1,6 @@
 import argparse
 import torch
+import os
 import numpy as np
 from matplotlib import pyplot as plt
 from models.vmamba import SS2D
@@ -19,8 +20,10 @@ def name_to_multiple_line(name, line_length=50):
     print(new_name)
     return new_name
 
-def plot_weight_distribution(model, model_name, bitwidth=32, plot_type='histogram',  per_channel=False):
+def plot_weight_distribution(model, model_name, bitwidth=32, plot_type='histogram'):
     # bins = (1 << bitwidth) if bitwidth <= 8 else 256
+    output_dir = f"{model_name}_weight_distribution"
+    os.makedirs(output_dir, exist_ok=True)
     if bitwidth <= 8:
         qmin, qmax = get_quantized_range(bitwidth)
         bins = np.arange(qmin, qmax + 2)
@@ -28,16 +31,15 @@ def plot_weight_distribution(model, model_name, bitwidth=32, plot_type='histogra
     else:
         bins = 256
         align = 'mid'
-    fig, axes = plt.subplots(21,10, figsize=(40, 24))
-    axes = axes.ravel()
     plot_index = 0
     for name, param in model.named_parameters():
         if not ('conv2d' in name or 'fc' in name):
             continue
         if param.dim() > 1:
-            ax = axes[plot_index]
-            if plot_type == 'histogram':
-                ax.hist(param.detach().view(-1).cpu(), bins=bins, density=True,
+            parmas_to_plot = param.detach().view(-1).cpu()
+            if 'histogram' in plot_type:
+                fig, ax = plt.subplots(figsize=(4, 3))
+                ax.hist(parmas_to_plot, bins=bins, density=True,
                         align=align, color = 'blue', alpha = 0.5,
                         edgecolor='black' if bitwidth <= 4 else None)
                 if bitwidth <= 4:
@@ -45,10 +47,40 @@ def plot_weight_distribution(model, model_name, bitwidth=32, plot_type='histogra
                     ax.set_xticks(np.arange(start=quantized_min, stop=quantized_max+1))
                 ax.set_xlabel(name_to_multiple_line(name))
                 ax.set_ylabel('density')
-            else:
-                ax.boxplot(param.detach().view(-1).cpu(), vert=False)
+                plt.tight_layout()
+                plt.savefig(os.path.join(output_dir, f'layer_{name}_histogram.png'))
+                plt.close(fig)  # Close the figure to free up memory
+
+            if 'box' in plot_type:
+                fig, ax = plt.subplots(figsize=(4, 3))
+                ax.boxplot(parmas_to_plot, vert=False)
                 ax.set_xlabel(name_to_multiple_line(name))
-            plot_index += 1
+                plt.tight_layout()
+                plt.savefig(os.path.join(output_dir, f'layer_{name}_box.png'))
+                plt.close(fig)  # Close the figure to free up memory
+            
+            # Plot channel-wise 3D weight distribution
+            if 'per_channel' in plot_type:
+                # if 'conv2d' in name:
+                #     print("here is conv2d")
+                #     torch.save(param, os.path.join(output_dir, f'{name}.pt'))
+                fig = plt.figure()
+                input_channel, output_channel = param.size()[:2]
+                x = np.linspace(0, input_channel, input_channel)
+                y = np.linspace(0, output_channel, output_channel)
+                x, y = np.meshgrid(y, x)
+                parmas_to_plot = param.detach().view(input_channel, output_channel, -1).mean(dim=2).abs().cpu()
+                
+                ax = fig.add_subplot(projection='3d')
+                ax.plot_surface(x, y, parmas_to_plot, cmap='coolwarm')
+                ax.set_title(name_to_multiple_line(name))
+                ax.set_xlabel('In Channel')
+                ax.set_ylabel('Out Channel')
+                ax.set_zlabel('Absolute Value')
+          
+                plt.tight_layout()
+                plt.savefig(os.path.join(output_dir, f'layer_{name}_per_channel.png'))
+                plt.close(fig)  # Close the figure to free up memory
             
 
     SS2D_weight_list = ["x_proj_weight", "dt_projs_weight", "dt_projs_bias", "A_logs", "Ds"]
@@ -56,8 +88,8 @@ def plot_weight_distribution(model, model_name, bitwidth=32, plot_type='histogra
         if isinstance(module, SS2D):
             for weight_name in SS2D_weight_list:
                 param = getattr(module, weight_name)
-                ax = axes[plot_index]
-                if plot_type == 'histogram':
+                if "histogram" in plot_type:
+                    fig, ax = plt.subplots(figsize=(4, 3))
                     ax.hist(param.detach().view(-1).cpu(), bins=bins, density=True,
                             align=align, color = 'blue', alpha = 0.5,
                             edgecolor='black' if bitwidth <= 4 else None)
@@ -66,13 +98,36 @@ def plot_weight_distribution(model, model_name, bitwidth=32, plot_type='histogra
                         ax.set_xticks(np.arange(start=quantized_min, stop=quantized_max+1))
                     ax.set_xlabel(name_to_multiple_line(f"{module_name}.{weight_name}"))
                     ax.set_ylabel('density')
-                else:
+                    plt.tight_layout()
+                    plt.savefig(os.path.join(output_dir, f'layer_{module_name}.{weight_name}_histogram.png'))
+                    plt.close(fig)  # Close the figure to free up memory
+                
+                if "box" in plot_type:
+                    fig, ax = plt.subplots(figsize=(4, 3))
                     ax.boxplot(param.detach().view(-1).cpu(), vert=False)
                     ax.set_xlabel(name_to_multiple_line(f"{module_name}.{weight_name}"))
+                    plt.tight_layout()
+                    plt.savefig(os.path.join(output_dir, f'layer_{module_name}.{weight_name}_box.png'))
+                    plt.close(fig)  # Close the figure to free up memory
+                
+                if "per_channel" in plot_type:
+                    fig = plt.figure()
+                    print("name", name, "param size", param.size())
+                    if len(param.size()) < 2:
+                        continue
+                    input_channel, output_channel = param.size()[:2]
+                    x = np.linspace(0, input_channel, input_channel)
+                    y = np.linspace(0, output_channel, output_channel)
+                    x, y = np.meshgrid(y, x)
+                    parmas_to_plot = param.detach().view(input_channel, output_channel, -1).mean(dim=2).abs().cpu()
+                    
+                    ax = fig.add_subplot(projection='3d')
+                    ax.plot_surface(x, y, parmas_to_plot, cmap='coolwarm')
+                    ax.set_title(name_to_multiple_line(f"{module_name}.{weight_name}"))
+                    ax.set_xlabel('In Channel')
+                    ax.set_ylabel('Out Channel')
+                    ax.set_zlabel('Absolute Value')
+                    plt.tight_layout()
+                    plt.savefig(os.path.join(output_dir, f'layer_{module_name}.{weight_name}_per_channel.png'))
+                    plt.close(fig)  # Close the figure to free up memory
                 plot_index += 1
-            
-    fig.suptitle(f'Weights distribution (histogram)')
-    fig.tight_layout()
-    fig.subplots_adjust(top=0.925)
-    plt.savefig(f'./{model_name}_weight_distribution_{plot_type}.png')
-
--- classification/run_draw_weight_distribution.sh
+++ classification/run_draw_weight_distribution.sh
@@ -1,16 +1,16 @@
 #! /bin/bash
 
 echo "Start draw weight distribution for tiny"
-python3 main_no_dist.py --cfg configs/vssm/vmambav2v_tiny_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_tiny_0230s_ckpt_epoch_264.pth --zip --plot_type histogram >> draw_weight_distribution.log 2>&1
-python3 main_no_dist.py --cfg configs/vssm/vmambav2v_tiny_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_tiny_0230s_ckpt_epoch_264.pth --zip --plot_type box >> draw_weight_distribution.log 2>&1
+python3 main_no_dist.py --cfg configs/vssm/vmambav2v_tiny_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_tiny_0230s_ckpt_epoch_264.pth --zip --plot_type histogram,box,per_channel >> draw_weight_distribution.log 2>&1
+zip -r vssm1_tiny_0230s_weight_distribution.zip vssm1_tiny_0230s_weight_distribution/
 echo "End draw weight distribution for tiny"
 
-echo "Start draw weight distribution for small"
-python3 main_no_dist.py --cfg configs/vssm/vmambav2v_small_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_small_0229s_ckpt_epoch_240.pth --zip --plot_type histogram >> draw_weight_distribution.log 2>&1
-python3 main_no_dist.py --cfg configs/vssm/vmambav2v_small_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_small_0229s_ckpt_epoch_240.pth --zip --plot_type box >> draw_weight_distribution.log 2>&1
+# echo "Start draw weight distribution for small"
+python3 main_no_dist.py --cfg configs/vssm/vmambav2v_small_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_small_0229s_ckpt_epoch_240.pth --zip --plot_type histogram,box,per_channel >> draw_weight_distribution.log 2>&1
+zip -r  vssm1_small_0229s_weight_distribution.zip vssm1_small_0229s_weight_distribution/
 echo "End draw weight distribution for small"
 
-echo "Start draw weight distribution for base"
-python3 main_no_dist.py --cfg configs/vssm/vmambav2v_base_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_base_0229s_ckpt_epoch_225.pth --zip --plot_type histogram >> draw_weight_distribution.log 2>&1
-python3 main_no_dist.py --cfg configs/vssm/vmambav2v_base_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_base_0229s_ckpt_epoch_225.pth --zip --plot_type box >> draw_weight_distribution.log 2>&1
+# echo "Start draw weight distribution for base"
+python3 main_no_dist.py --cfg configs/vssm/vmambav2v_base_224.yaml --data-path /joe/data/ImageNet-Zip --output /tmp/ --plot_weight_distribution --resume ckpt/vssm1_base_0229s_ckpt_epoch_225.pth --zip --plot_type histogram,box,per_channel >> draw_weight_distribution.log 2>&1
+zip -r vssm1_base_0229s_weight_distribution.zip vssm1_base_0229s_weight_distribution/
 echo "End draw weight distribution for base"
